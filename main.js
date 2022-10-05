db2d8dac667ecc73ece3e458025b89423dd945fb
Ezekiel Lopez <EzekielAdamLopez@Gmail.com>
2022-10-05 11:27:31 -0700

    added spacing around single line conditionals. Changed timing from performance.now to the event.timeStamp

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
 window.addEventListener("load", () => {
 
     const getEl = id => document.getElementById(id);
-    const getTime = () => performance.now();
 
     let beatStart, beatEnd, beatCount;
 
     const display = getEl("display");
     
     const updateDisplay = () => {
+
         const secondsPassed = (beatEnd - beatStart) / 1000;
         const bpm =  beatCount / secondsPassed * 60;
         display.innerText = bpm.toFixed(2);
+
     };
 
     const reset = () => {
+
         beatStart = undefined;
         beatEnd = undefined;
         beatCount = -1;
         display.innerText = "---.--";
+
     };
 
-    getEl("heart").addEventListener("click", () => {
-        if (beatStart === undefined) beatStart = getTime();
-        beatEnd = getTime();
+    getEl("heart").addEventListener("click", e => {
+        
+        if (beatStart === undefined) beatStart = e.timeStamp;
+
+        beatEnd = e.timeStamp;
+
         if (++beatCount) updateDisplay();
+
     });
 
     getEl("reset").addEventListener("click", reset);
